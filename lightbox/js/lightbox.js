c8966d6062e2b3c9c9ec82d4eeebb87303ec7a0f
Zaptrap <jpesante24@gmail.com>
2023-03-01 22:58:46 -0500

    lightbox

---
+++ lightbox/js/lightbox.js
@@ -0,0 +1,26 @@
+let lighboxContainer =
+    document.getElementById("lightboxContainer");
+
+let lightboxBackground =
+    document.getElementById("lightboxBackground");
+
+let lightbox = document.getElementById("lightbox");
+
+let lightboxCloser =
+    document.getElementById("lightboxCloser");
+
+function openLightbox() {
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage = document.getElementById("coffeeImage");
+coffeeImage.onclick = openLightbox; 
+
+function closeLightbox() {
+    lighboxContainer.classList.remove("display")
+    lightbox.src= "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
